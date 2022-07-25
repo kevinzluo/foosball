e1fd6a31758a327775e8214c1cc8344e8e733e58
Kevin Luo <44309027+kevinzluo@users.noreply.github.com>
2022-07-25 23:35:45 +0200

    Refactoring the gif generation - changed command line flags     - `-fancy` turns on fancy plots     - `-gif` turns on gif, if 0 it is just the final png     - `-recency` defines the number of past games to take players from     - `-past_games` defines the number of games to plot

--- plotter.py
+++ plotter.py
@@ -1,3 +1,4 @@
+from os import minor
 import matplotlib.pyplot as plt
 from matplotlib import animation, cm
 import pandas as pd
@@ -6,7 +7,7 @@ import copy
 from rank_players import get_game_teams
 from util import ELO_GRAPH_DIR
 from scipy.interpolate import make_interp_spline
-
+import matplotlib.ticker as ticker
 def plot_elo_history(player, games, elo_history):
     def check_if_played(game):
         game_teams = get_game_teams(game)
@@ -29,8 +30,10 @@ def plot_elo_history(player, games, elo_history):
     ax1.plot(updated_games, player_elo, '-o')
     fname = ELO_GRAPH_DIR / "{}_elo_history.png".format(player)
     plt.savefig(fname)
+    plt.close()
 
-def anim_elo_history(players, games, elo_history):
+def anim_elo_history(players, games, elo_history, recency, games_to_plot, gif):
+    print(pd.DataFrame(elo_history))
     # get elo as df with columns player names
     trajs = pd.DataFrame(elo_history).T
     # append start rating of 1000 for players of the first game
@@ -45,39 +48,54 @@ def anim_elo_history(players, games, elo_history):
     num_games = len(games)
     games.insert(0, {})
 
+    if recency < 0:
+        recency = num_games + 1
+    if games_to_plot < 0:
+        games_to_plot = num_games + 1
+
+    START_GAME = max(0, num_games - games_to_plot)
+
     # get first game played
     first_games = {}
+    last_games = {}
     for player in players:
         for i, game in enumerate(games):
             game_teams = get_game_teams(game)
             if (player in game_teams[0]) | (player in game_teams[1]):
-                first_games[player] = i 
-                break
+                if player not in first_games:
+                    first_games[player] = i
+                last_games[player] = i
 
+    players = players.copy()
+    players = [player for player in players if num_games - last_games[player] < recency]
+    min_first_game = min([v for _, v in first_games.items()])
+    START_GAME = min(min_first_game, START_GAME)
     # fixed at 30FPS
-    INTERVAL = 1 / 60 * 1000 # not actually matters
     FPS = 30
     TOTAL_RUNTIME = 5 # in seconds
-    FRAMES_PER_GAME = TOTAL_RUNTIME * FPS // (num_games + 1)
+    FRAMES_PER_GAME = (TOTAL_RUNTIME * FPS) // (num_games + 1)
     plt.close('all')
-    fig, ax = plt.subplots(1, 1, figsize = (6, 4))
+    # height of 6 is comfortable for 25 players, just scaling
+    fig, ax = plt.subplots(1, 1, figsize = (8, max(4, 6 * len(players) / 25)))
 
     EASE_IN_FUNC = lambda t: t ** 2
     EASE_OUT_FUNC = lambda t: (1 - t) ** 2
 
-    BASE_MARKERSIZE = 18
-    MAX_ADDL_MARKERSIZE = 36
+    BASE_MARKERSIZE = 0
+    MAX_ADDL_MARKERSIZE = 54
 
-    ## TODO: THIS BREAKS ONCE 20 PLAYERS
-    cycle = cm.tab20(np.linspace(0, 1, len(players), endpoint = False)) 
+    ## TODO: THIS BREAKS ONCE 80 PLAYERS
+    color_cycle = cm.tab20(np.linspace(0, 1, len(players), endpoint = False)) 
+    linestyle_cycle = ['solid', 'dashed', 'dashdot', 'dotted']
 
     # precompute splines, markers, and thicknesses
     paths = {}
     for player in players:
+        SEG_RESOLUTION = max(int(3 * FRAMES_PER_GAME), 40)
         # support for x axis, take game before to start at 1000
         xsup = np.arange(first_games[player] - 1, num_games + 1)
         # finer grain for spline, with one increment per frame
-        xnew = np.linspace(min(xsup), max(xsup), FRAMES_PER_GAME * (len(xsup) - 1) + 1)
+        xnew = np.linspace(min(xsup), max(xsup), SEG_RESOLUTION * (len(xsup) - 1) + 1)
 
         # get elos for the player over their support
         trunc_traj = trajs[player][xsup]
@@ -91,10 +109,10 @@ def anim_elo_history(players, games, elo_history):
         
         # for endpoint marker thickness
         # we ignore the very first point of xnew, since it always an unplayed game
-        inst_thickness = np.zeros((2, FRAMES_PER_GAME * (len(xsup) - 1)))
+        inst_thickness = np.zeros((2, SEG_RESOLUTION * (len(xsup) - 1)))
 
         # sample the right endpoint of every interval of [0, 1]
-        delta, delta_range = np.linspace(0, 1, FRAMES_PER_GAME, endpoint=False, retstep=True)
+        delta, delta_range = np.linspace(0, 1, SEG_RESOLUTION, endpoint=False, retstep=True)
         delta_range += delta
 
         up_section = EASE_IN_FUNC(delta_range)
@@ -103,14 +121,14 @@ def anim_elo_history(players, games, elo_history):
         for game_num, in_game in zip(xsup, in_game_indicator):
             if game_num == 0:
                 continue
-            entry_index = FRAMES_PER_GAME * (game_num - (first_games[player] - 1))
+            entry_index = SEG_RESOLUTION * (game_num - (first_games[player] - 1))
             if in_game:
                 # queue up upscaling for this 
-                inst_thickness[0, entry_index - FRAMES_PER_GAME:entry_index] = up_section
+                inst_thickness[0, entry_index - SEG_RESOLUTION:entry_index] = up_section
 
                 # queue up downscaling for the next section
                 if game_num < num_games:
-                    inst_thickness[1, entry_index:entry_index + FRAMES_PER_GAME] = down_section
+                    inst_thickness[1, entry_index:entry_index + SEG_RESOLUTION] = down_section
 
         inst_thickness = inst_thickness.max(axis = 0)
         # removed to use this adjustment for markers
@@ -131,46 +149,44 @@ def anim_elo_history(players, games, elo_history):
 
         return curr_xsup, curr_trunc_traj, curr_marker_sizes, curr_xnew, curr_spline_path, curr_inst_thickness
 
-
     # animation for frame i
     def animate(i):
         # clear plot
         ax.cla()
 
-        # time starts at 0 and ends at len(games) (+1 more second of freeze at end)
-        curr_time_as_game = i / FRAMES_PER_GAME
+        curr_time_as_game = START_GAME + i / (5 * FPS) * (num_games + 1 - START_GAME)
 
-        for i, player in enumerate(players):
+        for j, player in enumerate(players):
             # filter to only plot up to current time
             curr_xsup, curr_trunc_traj, curr_marker_sizes, curr_xnew, curr_spline_path, curr_inst_thickness = return_at_time(player, curr_time_as_game)
 
             # append on point in current time to have line end with a dot
+            color = color_cycle[j % len(color_cycle)]
+            linestyle = linestyle_cycle[j // len(color_cycle)]
+
             if curr_inst_thickness.shape[0] > 0:
-                ax.scatter(list(curr_xsup) + [curr_xnew[-1]], list(curr_trunc_traj) + [curr_spline_path[-1]], label = player,
+                ax.scatter(list(curr_xsup) + [curr_xnew[-1]], list(curr_trunc_traj) + [curr_spline_path[-1]], 
                         s = list(curr_marker_sizes) + [BASE_MARKERSIZE + curr_inst_thickness[-1] * MAX_ADDL_MARKERSIZE], 
                         alpha = 0.8,
-                        color = cycle[i])
+                        color = color)
             
-                ax.plot(curr_xnew, curr_spline_path, color = cycle[i], alpha = 0.8)
-
+                ax.plot(curr_xnew, curr_spline_path, color = color, linestyle = linestyle, label = player, alpha = 0.8)
             else:
-                ax.scatter([], [], label = player,
-                        alpha = 0.8,
-                        color = cycle[i])
+                ax.plot([], [], color = color, linestyle = linestyle, label = player, alpha = 0.8)
 
 
         # fix x-axis 
         # possible change: sliding x/y axis
-        ax.set_xlim([0, num_games])
-        Y_MIN = trajs.min().min()
-        Y_MAX = trajs.max().max()
+        ax.set_xlim([START_GAME, num_games])
+        Y_MIN = trajs.loc[START_GAME:].min().min()
+        Y_MAX = trajs.loc[START_GAME:].max().max()
         Y_RANGE = Y_MAX - Y_MIN
         ax.set_ylim([Y_MIN - Y_RANGE / 10, Y_MAX + Y_RANGE / 10])
         ax.legend(loc = "center left", bbox_to_anchor = (1, 0.5))
         fig.tight_layout()
-
-    # animate(15)
-    # plt.show()
-
-    anim = animation.FuncAnimation(fig, animate, frames = range(1, FRAMES_PER_GAME * num_games + FPS + 1), interval = INTERVAL / 10, blit = False)
-    anim.save("elo_graphs/elo.gif", animation.FFMpegWriter(fps = FPS))
+    if gif == 1:
+        anim = animation.FuncAnimation(fig, animate, frames = range(1, FPS * (TOTAL_RUNTIME + 3) + 1), blit = False)
+        anim.save("elo_graphs/elo.gif", animation.FFMpegWriter(fps = FPS))
+    else:
+        animate(FPS * (TOTAL_RUNTIME + 3))
+        fig.savefig("elo_graphs/elo.png", bbox_inches = "tight")
--- runner.py
+++ runner.py
@@ -4,7 +4,10 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate ELO graphics")
+    parser.add_argument("-fancy", default=0)
     parser.add_argument("-gif", default=0)
+    parser.add_argument("-recency", default = 30, help = "only show players who've played in one of the last `recency` games; -1 to plot all players")
+    parser.add_argument("-past_games", default = -1, help = "only plot the last `past_games` games; -1 to plot all games")
 
     args = parser.parse_args()
     pull_table()
@@ -16,5 +19,5 @@ if __name__ == "__main__":
     for player in players:
         plot_elo_history(player, games, elo_history)
     
-    if int(args.gif) == 1:  
-        anim_elo_history(players, games, elo_history)
+    if int(args.fancy) == 1:  
+        anim_elo_history(players, games, elo_history, int(args.recency), int(args.past_games), int(args.gif))
